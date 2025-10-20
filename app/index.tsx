86ca557bf6b8998a7d7e70c8c1dc209bab393381
Sanjay Baskar <sanjaybaskar.in@gmail.com>
2025-10-20 21:36:05 +0530

    added working tailwindcss , added sample materials, working error less code - initial setup 2

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,25 @@
-import "./globals.css"
-import { Text, View } from "react-native";
+import {SplashScreen, Stack} from "expo-router";
+import './globals.css';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import './globals.css';
 
-export default function Index() {
+export default function RootLayout() {
+    // functional
+    const [fontsLoaded, error] = useFonts({
+        "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+        "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+        "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+        "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+        "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf')
+    })
+
+    useEffect(() => {
+        if (error) throw error;
+        if (fontsLoaded) SplashScreen.hideAsync(); // helps loader	// coming from expo router
+    }, [fontsLoaded, error]); // any change load again means
+
     return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-xl font-bold text-blue-500">
-                Welcome to Nativewind!
-            </Text>
-        </View>
+        <Stack screenOptions={{ headerShown: false }} />
     );
 }
